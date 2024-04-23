c487ba567d94b6f89c91221cdb0a53637a195576
lewisdavidson <lewisdavidson8@gmail.com>
2024-04-23 07:34:07 +0100

    docs: document random utility functions

--- src/lib/utils/random.ts
+++ src/lib/utils/random.ts
@@ -1,7 +1,18 @@
+/**
+ * Picks a random element from the provided array.
+ * @param {T[]} array - The array to pick a random element from.
+ * @returns {T} A random element from the array.
+ */
 export function randomPick<T>(array: T[]): T {
 	return array[Math.floor(Math.random() * array.length)];
 }
 
+/**
+ * Generates a random integer within the provided range (inclusive).
+ * @param {number} min - The minimum possible value.
+ * @param {number} max - The maximum possible value.
+ * @returns {number} A random integer within the specified range.
+ */
 export function randomInt(min: number, max: number): number {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
