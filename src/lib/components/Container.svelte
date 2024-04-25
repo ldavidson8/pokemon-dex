4d5ac3ed1e4dfdbc259341ce9aeff7ad1006d6c3
lewisdavidson <lewisdavidson8@gmail.com>
2024-04-25 08:19:35 +0100

    Updated container component to accept classes.

--- src/lib/components/Container.svelte
+++ src/lib/components/Container.svelte
@@ -1,3 +1,11 @@
-<div class="grid-rows[auto,1fr] container grid grid-cols-1">
+<script lang="ts">
+	import { cn } from '$lib/utils/cn';
+
+	export let className: string = '';
+
+	export { className as class };
+</script>
+
+<div class={cn('grid-rows[auto,1fr] container grid grid-cols-1', className)}>
 	<slot />
 </div>
